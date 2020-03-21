d4ad38255a3f7a84f3763083c6d26e52759432b2
Renuka Misal <renuka.misal@rksv.in>
2020-03-21 15:26:39 +0530

    Added version of python sdk.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'upstox',
   packages = ['upstox_api', 'upstox_api.constants'],
-  version = '1.5.93',
+  version = '2.0.1',
   include_package_data=True,
   description = 'Official Python library for Upstox APIs Staging Environment',
   author = 'Upstox Development Team',
