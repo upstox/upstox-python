f3814ca18e5a1f98f07bf5a4fefadfbb8e4e8463
KETAN GUPTA <ketan.gupta@rksv.in>
2025-07-02 18:13:10 +0530

    updated version

--- upstox_client/api_client.py
+++ upstox_client/api_client.py
@@ -74,7 +74,7 @@ class ApiClient(object):
         # Set default User-Agent.
         self.user_agent = 'Swagger-Codegen/1.0.0/python'
         self.default_headers["X-Upstox-SDK-Language"] = "python"
-        self.default_headers["X-Upstox-SDK-Version"] = "2.16.0"
+        self.default_headers["X-Upstox-SDK-Version"] = "2.17.0"
 
     def __del__(self):
         try:
