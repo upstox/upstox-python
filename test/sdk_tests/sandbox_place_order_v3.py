ffbc9dca592bcbf9010b286fad206d1cb7b0199d
KETAN GUPTA <ketan.gupta@rksv.in>
2026-03-10 16:03:48 +0530

    Add market protection parameter to order-related models and update tests accordingly

--- test/sdk_tests/sandbox_place_order_v3.py
+++ test/sdk_tests/sandbox_place_order_v3.py
@@ -5,7 +5,7 @@ configuration = upstox_client.Configuration(sandbox=True)
 configuration.access_token = 'SANDBOX_ACCESS_TOKEN'
 
 api_instance = upstox_client.OrderApiV3(upstox_client.ApiClient(configuration))
-body = upstox_client.PlaceOrderV3Request(quantity=1, product="D",validity="DAY", price=9.12, tag="string", instrument_token="NSE_EQ|INE669E01016", order_type="LIMIT",transaction_type="BUY", disclosed_quantity=0, trigger_price=0.0, is_amo=True, slice=True)
+body = upstox_client.PlaceOrderV3Request(quantity=1, product="D", validity="DAY", price=9.12, tag="string", instrument_token="NSE_EQ|INE669E01016", order_type="LIMIT", transaction_type="BUY", disclosed_quantity=0, trigger_price=0.0, is_amo=True, slice=True, market_protection=0)
 
 try:
     api_response = api_instance.place_order(body)
